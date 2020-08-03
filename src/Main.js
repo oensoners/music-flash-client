f25b5c51fc38bc1640e536b03996a4ccc2713b7c
Flash KIM <62537294+Flash-KIM142@users.noreply.github.com>
2020-08-03 18:15:04 +0900

    create Main.js

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Content from './components/main/Content'; 
+import './styles/main.css';
+
+
+function Main() {
+  return (
+    <div className="main">
+      <Content/>
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/components/main/Content.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../styles/content.css';
+import Buttons from '../Buttons';
+const Content = () =>{
+
+
+
+
+
+    return (
+        <div className="content">
+            <button>Albums</button>
+            <button>Musicians</button>
+            {/* <Buttons content="Albums"/>
+            <Buttons content="Musicians"/> */}
+            
+        </div>   
+    );
+
+}
+
+export default Content;
