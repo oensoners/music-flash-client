86392e3d34113b8a76b5917a9770573ea7bb375a
Flash KIM <62537294+Flash-KIM142@users.noreply.github.com>
2020-08-11 19:01:23 +0900

    header init

--- src/components/main/Content.js
+++ src/components/main/Content.js
@@ -7,21 +7,44 @@ const Content = () =>{
 
 
     return (
-        <div className="content">
-            {/* <button className="list">Albums</button>
-            <button className="list" style={{marginTop:'30px'}}>Musicians</button> */}
-
-            <div className="container">
-                <div className="home-categories">
-                    <div className="category-text-wrap">
-                        <span className="category-text">Albums</span>
-                    </div>
-                    <div className="category-text-wrap">
-                        <span className="category-text">Musicians</span>
+        <div className="home">
+            <div className="texture-black">
+                <div className="header-box">
+                    <div className="navbar">
+                        <ul>
+                            <li>Home</li>
+                            <li>Profile</li>
+                        </ul>
                     </div>
                 </div>
-            </div>            
-        </div>   
+                <div className="container">
+                    <h1 className="header-me">Flash KIM</h1>
+                    <h3 className="header-me">Project Manager. Developer. Designer</h3>
+                    <ul className="social">
+                        <a href="https://github.com/Flash-KIM142">
+                            <li>
+                                <i className="icon-github-circled"
+                                    src="../../assets/image/icon-social-github.png"
+                                    alt="github-icon"></i>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="content">
+                <div className="container">
+                    <div className="home-categories">
+                        <div className="category-text-wrap">
+                            <span className="category-text">Albums</span>
+                        </div>
+                        <div className="category-text-wrap">
+                            <span className="category-text">Musicians</span>
+                        </div>
+                    </div>
+                </div>            
+            </div>   
+        </div>
     );
 
 }
